6b41c33614a35bdc785834a76dc384ac68eb1a63
faizaibrahim22 <faizaibrahim14789@gmail.com>
2024-03-24 16:31:30 -0700

    name

---
+++ Desktop/percentage/main.js
@@ -0,0 +1,16 @@
+function result(num) {
+    if (num >= 99) {
+        console.log("A+");
+    }
+    else if (num >= 80) {
+        console.log("A");
+    }
+    else if (num >= 70) {
+        console.log("B");
+    }
+    else {
+        console.log("fail");
+    }
+    return (num);
+}
+result(77);
