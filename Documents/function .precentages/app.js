13f53328261d303fbbb7b5630f441e3ec90a010b
faizaibrahim22 <faizaibrahim14789@gmail.com>
2024-03-25 15:24:29 -0700

    'name'

---
+++ Documents/function .precentages/app.js
@@ -0,0 +1,17 @@
+function percentage(num) {
+    if (num >= 90) {
+        console.log("A+");
+    }
+    else if (num >= 80) {
+        console.log("A");
+    }
+    else if (num >= 70) {
+        console.log("B");
+    }
+    else {
+        console.log("fail");
+    }
+    return (num);
+}
+percentage(99);
+export {};
